00a943ba6a630c78ab654214ab592bdebea8fe1f
MIGUEL ANGEL DELGADO VILLAFAN <miguel.delgado@banregio.com>
2023-06-18 20:24:05 -0600

    dd

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ let express = require('express');
 
 let app = express();
 
-app.use(express.static(__dirname+'/dist/aplicacionEmpresarial'));
+app.use(express.static(__dirname+'/dist/aplicacion-empresarial'));
 
 app.get('/*', (req, resp)=>{
-    resp.sendFile(__dirname+'/dist/aplicacionEmpresarial/index.html');
+    resp.sendFile(__dirname+'/dist/aplicacion-empresarial/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
